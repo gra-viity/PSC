72e1ece699c24fb4427f087a90981333bb414ff9
gra-viity <shubham.rawat1995@gmail.com>
2020-09-06 01:34:36 +0530

    Login screen added

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1200);
+    }
+}
